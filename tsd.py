0ce49bd92be50d09c1b313c100093410db0ae9fa
aidatorajiro <kawarusosu@zoho.com>
2020-06-19 23:02:43 +0900

    add tsd.py

---
+++ tsd.py
@@ -0,0 +1,24 @@
+## general purpose ts donwloader
+
+with open("in.har") as f:
+    data = f.read()
+
+prs = json.loads(data)
+
+import re, base64
+
+streams = []
+
+for e in prs['log']['entries']:
+    url = e['request']['url']
+    if re.match(r".+/.+?\.ts", url):
+        cont = e['response']['content']
+        stream = cont['text']
+        if cont['encoding'] == 'base64':
+            stream = base64.b64decode(stream)
+        streams.append(stream)
+
+concat = b"".join(streams)
+
+with open("out.ts", "wb") as f:
+    f.write(concat)
