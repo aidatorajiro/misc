9868159c8030f1214fbb9772aae23fa710f14340
aidatorajiro <kawarusosu@zoho.com>
2020-06-25 16:18:09 +0900

    add nanika and update tsd.py

--- tsd.py
+++ tsd.py
@@ -1,8 +1,19 @@
-## general purpose ts donwloader
+## Convert a HAR file to a concatenated sequence of matched packets.
 
-import re, base64, json
+import re, base64, json, argparse
 
-with open("in.har") as f:
+parser = argparse.ArgumentParser(description='Convert a HAR file to the concatenation of the matched packets.')
+
+parser.add_argument('in_filename', nargs='?', default="in.har", metavar='in', type=str, help='input HAR file. default: in.har')
+parser.add_argument('out_filename', nargs='?', default="out.ts", metavar='out', type=str, help='output file. The content will be the concatenation of the matched packets. default: out.ts')
+parser.add_argument('stream_regex', nargs='?', default=r".+/.+?\.ts", metavar='regex', type=str, help=r'Regular expression to filter packets. default: .+/.+?\.ts')
+
+args = parser.parse_args()
+in_filename = args.in_filename
+out_filename = args.out_filename
+stream_regex = args.stream_regex
+
+with open(in_filename) as f:
     data = f.read()
 
 prs = json.loads(data)
@@ -12,7 +23,7 @@ streams = []
 
 for e in prs['log']['entries']:
     url = e['request']['url']
-    if re.match(r".+/.+?\.ts", url):
+    if re.match(stream_regex, url):
         cont = e['response']['content']
         stream = cont['text']
         if cont['encoding'] == 'base64':
@@ -21,5 +32,5 @@ for e in prs['log']['entries']:
 
 concat = b"".join(streams)
 
-with open("out.ts", "wb") as f:
+with open(out_filename, "wb") as f:
     f.write(concat)
