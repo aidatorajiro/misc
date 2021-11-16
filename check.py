e5148f42c41c861b350b19bf8d3ba0b6fc5c62f7
aidatorajiro <kawarusosu@zoho.com>
2021-11-17 01:13:22 +0900

    check & tsd: stop int() function use and use string sort

--- check.py
+++ check.py
@@ -3,11 +3,11 @@ import re, base64, json, argparse
 parser = argparse.ArgumentParser(description='Check HAR file integrity.')
 
 parser.add_argument('in_filename', nargs='+', default="in.har", metavar='in', type=str, help='input HAR file. default: in.har')
-parser.add_argument('num_regex', nargs='?', default=r".+/(\d+)\.ts", metavar='sort_regex', type=str, help=r'A regular expression to get numeric values to sort packets.')
+parser.add_argument('regex', nargs='?', default=r".+/(\d+)\.ts", metavar='sort_regex', type=str, help=r'A regular expression to obtain some part of URL and sort packets.')
 
 args = parser.parse_args()
 in_filename = args.in_filename
-num_regex = args.num_regex
+regex = args.regex
 
 log_entries = []
 
@@ -18,10 +18,11 @@ for fn in in_filename:
 
 streams = []
 
-entries = list(filter(lambda x: x[0] != None and 'text' in x[1]['response']['content'],
-    [[re.match(num_regex, x['request']['url']), x] for x in log_entries]))
+entries = list(filter(
+    lambda x: x[0] != None and 'text' in x[1]['response']['content'],
+    [[re.match(regex, x['request']['url']), x] for x in log_entries]))
 
-pairs = sorted([[int(x[0][1]), x[1]] for x in entries], key=lambda x: x[0])
+pairs = sorted([[x[0][1], x[1]] for x in entries], key=lambda x: [len(x[0]), x[0]])
 
 pairs_processed = []
 last = None
@@ -33,5 +34,6 @@ for x, y in pairs:
 print("Start = %s" % pairs_processed[0][0])
 print("End = %s" % pairs_processed[-1][0])
 print("Len = %s" % len(pairs_processed))
+print([t[0] for t in pairs_processed])
 
-print(len(pairs_processed) == pairs_processed[-1][0] - pairs_processed[0][0] + 1)
+# print(len(pairs_processed) == pairs_processed[-1][0] - pairs_processed[0][0] + 1)
--- tsd.py
+++ tsd.py
@@ -7,17 +7,17 @@ parser = argparse.ArgumentParser(description='Convert a HAR file to the concaten
 parser.add_argument('infiles', nargs='+', default="in.har", metavar='infiles', type=str, help='input HAR file. default: in.har')
 parser.add_argument('--out', default="out.ts", metavar='out', type=str, help='output file. The content will be the concatenation of the matched packets. default: out.ts')
 parser.add_argument('--stream_regex', default=r".+/\d+\.ts", metavar='stream_regex', type=str, help=r'A regular expression to filter packets.')
-parser.add_argument('--numsort', default=True, action=argparse.BooleanOptionalAction, help=r'Whether to use numeric sort based on URL.')
-parser.add_argument('--numnodup', default=True, action=argparse.BooleanOptionalAction, help=r'Whether to take only one request by each number. Requires numsort is true.')
-parser.add_argument('--numsort_regex', default=r".+/(\d+)\.ts", metavar='numsort_regex', type=str, help=r'A regular expression to get numeric values to sort packets.')
+parser.add_argument('--lensort', default=True, action=argparse.BooleanOptionalAction, help=r'Whether to use length-priority sort based on URL.')
+parser.add_argument('--sort_nodup', default=True, action=argparse.BooleanOptionalAction, help=r'Whether to take only one request by each number. Requires lensort is true.')
+parser.add_argument('--sort_regex', default=r".+/(\d+)\.ts", metavar='numsort_regex', type=str, help=r'A regular expression to get numeric values to sort packets.')
 
 args = parser.parse_args()
 infiles = args.infiles
 out_filename = args.out
 stream_regex = args.stream_regex
-numsort = args.numsort
-numnodup = args.numnodup
-numsort_regex = args.numsort_regex
+lensort = args.lensort
+sort_nodup = args.sort_nodup
+sort_regex = args.sort_regex
 
 log_entries = []
 
@@ -30,33 +30,34 @@ streams = []
 
 entries = list(filter(lambda x: re.match(stream_regex, x['request']['url']) and 'text' in x['response']['content'], log_entries))
 
-if numsort:
+if lensort:
     pairs = sorted(
             zip(
-                [int(re.match(numsort_regex, x['request']['url'])[1])for x in entries]
+                [re.match(sort_regex, x['request']['url'])[1] for x in entries]
                 , entries
             )
-            , key=lambda x: x[0]
+            , key=lambda x: [len(x[0]), x[0]]
         )
     
-    if numnodup:
+    if sort_nodup:
         pairs_processed = []
         last = None
         for x, y in pairs:
             if last != x:
-                print(x)
+                print("accepted: " + x)
                 pairs_processed.append([x, y])
                 last = x
+            else:
+                print("duplicate omitted: " + x)
     else:
         pairs_processed = pairs
     
     entries = [x[1] for x in pairs_processed]
 
 for e in entries:
-    # print(int(re.match(numsort_regex, e['request']['url'])[1]))
+    # print(int(re.match(sort_regex, e['request']['url'])[1]))
     cont = e['response']['content']
-    if 'text' in cont:
-        stream = cont['text']
+    stream = cont['text']
     if 'encoding' in cont and cont['encoding'] == 'base64':
         stream = base64.b64decode(stream)
     streams.append(stream)
