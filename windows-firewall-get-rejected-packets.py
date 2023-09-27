65094bf9ec5f8a6aa1498ac5b70d2c3ea27fc005
aidatorajiro <kawarusosu@zoho.com>
2023-09-27 15:43:42 +0900

    add some commentto windows firewall script

--- windows-firewall-get-rejected-packets.py
+++ windows-firewall-get-rejected-packets.py
@@ -31,11 +31,14 @@ EVT_SUBSCRIBE_CALLBACK = CFUNCTYPE(c_uint, c_uint, c_void_p, c_ulonglong)
 deny_log = []
 
 def py_callback(code, ctx, ev_handle):
+    """
+    Callback function for catching system Security events.
+    """
     buf_size = c_int(0)
     prop_vount = c_int(0)
-    winevt.EvtRender(0, ev_handle, 1, 0, 0, pointer(buf_size), pointer(prop_vount))
+    winevt.EvtRender(0, c_ulonglong(ev_handle), 1, 0, 0, pointer(buf_size), pointer(prop_vount))
     xml_data = create_unicode_buffer(buf_size.value)
-    success = winevt.EvtRender(0, ev_handle, 1, buf_size, pointer(xml_data), pointer(buf_size), pointer(prop_vount))
+    success = winevt.EvtRender(0, c_ulonglong(ev_handle), 1, buf_size, pointer(xml_data), pointer(buf_size), pointer(prop_vount))
     if not success:
         print_err_for_evt()
     else:
@@ -68,9 +71,15 @@ def py_callback(code, ctx, ev_handle):
     return 1
 
 def isSvcHostService(app_info):
+    """
+    Check if the process is a service hosted by svchost.exe (in that case we also have to obtain Service Name to identify which module is responsible)
+    """
     return re.match(r'^\\device\\harddiskvolume\d+\\windows\\system32\\svchost.exe$', app_info['Application'])
 
 def print_err_for_evt():
+    """
+    Print Windows Event API's error message
+    """
     e = k32.GetLastError()
     bufsize = c_int(1) # i think this is Windows bug somewhere...
     strdata = create_unicode_buffer('', bufsize.value)
@@ -80,10 +89,16 @@ def print_err_for_evt():
     print("!!!ERROR!!!", "CODE", e, "MESSAGE", strdata.value)
 
 def print_err_generic():
+    """
+    Print Windows generic error message
+    """
     e = k32.GetLastError()
     print("!!!ERROR!!!", "CODE", e)
 
 def signal_handler(sig, frame):
+    """
+    Handler function on proces exit
+    """
     print()
     subprocess.run(['auditpol.exe', '/set', '/subcategory:{0CCE9225-69AE-11D9-BED3-505054503030}', '/success:disable', '/failure:disable'])
     sys.exit(0)
@@ -107,9 +122,15 @@ class ENUM_SERVICE_STATUS_PROCESSW(Structure):
             ("ServiceStatusProcess", SERVICE_STATUS_PROCESS)]
 
 def convert_type(value, totype):
+    """
+    Convert a ctype object's type into another
+    """
     return cast(pointer(value), POINTER(totype)).contents
 
 def enum_services():
+    """
+    Return all running services using EnumServicesStatusExW
+    """
     # maybe 0x3B? idk
     bufsize = c_int(0)
     entries = c_int(0)
@@ -130,15 +151,19 @@ def enum_services():
 # ++++++
 
 if __name__ == '__main__':
+    # enable security audit event for packet rejection
     subprocess.run(['auditpol.exe', '/set', '/subcategory:{0CCE9225-69AE-11D9-BED3-505054503030}', '/success:disable', '/failure:enable'])
 
+    # create callback object
     callback = EVT_SUBSCRIBE_CALLBACK(lambda x, y, z: py_callback(x, y, z))
 
+    # subscribe to security events
     winevt.EvtSubscribe.restype = c_ulonglong
     ev_handle_main = winevt.EvtSubscribe(None, None, r"Security", None, None, None, callback, 1)
     if ev_handle_main == 0:
         print_err_for_evt()
 
+    # open service manager to enable getting the list of services
     database_name = create_unicode_buffer("ServicesActive")
     advapi32.OpenSCManagerW.restype = c_longlong
     service_manager = advapi32.OpenSCManagerW(0, pointer(database_name), 0x20014)
