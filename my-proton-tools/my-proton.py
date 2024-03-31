da5f30fc312a90294853d6ad1868b7fe5553f557
aidatorajiro <kawarusosu@zoho.com>
2024-03-31 20:43:14 +0900

    update proton-tool to work with older proton

--- my-proton-tools/my-proton.py
+++ my-proton-tools/my-proton.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 
+PRESET_PATH_DEFAULT = 'dumpenvs_preset_minimum'
+
 manual = """
 This program allows you to run any exe files in a custom wine prefix. 
 
 [Instruction]
+
 1. start a steam app via proton, and get its pid
 2. `cat /proc/[pid of the game]/environ > dumpenvs` in the directory this program exists
 3. run this program once and generate `dumpenvs_keys` and `dumpenvs_keys_values`
 4. copy `dumpenvs_keys_values` and rename it.
 5. edit the copied file, replacing string values next to the keys with 'v', 'p', 'c' or 'd' or deleting the line
    v = keep value, c = create cache dir (WIP), d = delete value
-6. set `PRESET_PATH` in this script to the path of edited file, relative to the program location
+6. by setting `PRESET_PATH_DEFAULT` in this script or passing `-p` argument, specify the path of edited file, relative to the program location.
 7. copy existing proton prefix located at `~/.local/share/Steam/steamapps/compatdata/[game id]` into `~/wineprefix/[wineprefix name]`
+
+[Note]
+
+You can change proton version by specifying --proton and --runtime simultaneously,
+Also, please dump separate env file and change --dumpenv accordingly if you use multiple proton versions...
 """
 
 import os
@@ -28,9 +36,15 @@ parser.add_argument('prefixname', help='The name of wineprefix. The wineprefix s
 
 parser.add_argument('args', nargs='*', help='Args to be passed on wine or executed directly')
 
-parser.add_argument('-p', '--preset', help="path to the preset file, relative to this script. (eg. dumpenvs_preset_minimum)")
+parser.add_argument('-p', '--preset', help="path to the preset file, relative to this script.", default='dumpenvs_preset_minimum')
 
-parser.add_argument('-l', '--lang', help="wine language (eg. ja_JP.UTF-8)")
+parser.add_argument('-l', '--lang', help="wine language", default='ja_JP.UTF-8')
+
+parser.add_argument('--runtime', help="Specify steam linux runtime container executable path relative to steamapps.", default='SteamLinuxRuntime_sniper/run-in-sniper')
+
+parser.add_argument('--proton', help="Specify Proton Version.", default='Proton - Experimental')
+
+parser.add_argument('--dumpenvs', help="Specify path to dumped env file (by copying /proc/xxx/environ).", default='dumpenvs')
 
 parser.add_argument('-r', help="execute raw command such as `winetricks` or `wineserver -k`", action='store_true')
 
@@ -42,17 +56,18 @@ else:
     args = parser.parse_args()
 
 PFX_TO_BE_MADE = args.prefixname
-PRESSURE_PATH = os.path.expanduser("~/.local/share/Steam/ubuntu12_64/steam-runtime-sniper/var/tmp-SGAKL2/")
-PRESET_PATH = args.preset or 'dumpenvs_preset_minimum'
-SPIDER_PATH = os.path.expanduser('~/.local/share/Steam/steamapps/common/SteamLinuxRuntime_sniper/run-in-sniper')
+PRESET_PATH = args.preset
+SNIPER_PATH = os.path.expanduser('~/.local/share/Steam/steamapps/common/%s' % args.runtime)
 
 tool_path = os.path.abspath(os.path.dirname(__file__))
 
-proton_root = os.path.expanduser("~/.local/share/Steam/steamapps/common/Proton - Experimental/")
+proton_root = os.path.expanduser("~/.local/share/Steam/steamapps/common/%s/" % args.proton)
 if not os.path.exists(proton_root):
     proton_root = "/usr/share/steam/compatibilitytools.d/proton"
+    if not os.path.exists(proton_root):
+        raise FileNotFoundError('Proton executable not found.')
 
-with open(os.path.join(tool_path, 'dumpenvs'), 'rb') as f:
+with open(os.path.join(tool_path, args.dumpenvs), 'rb') as f:
     dumpenvs = [x.split(b'=', 1) for x in f.read().split(b'\0') if x != b'']
 
 with open(os.path.join(tool_path, 'dumpenvs_keys_values'), 'w') as f:
@@ -66,6 +81,9 @@ final_env = {}
 # v: copy value
 # c: cache (specify cache path at next arg)
 # d: delete
+# r: replace with some value
+# a: append some value
+# p: prepend some value
 
 with open(os.path.join(tool_path, PRESET_PATH)) as f:
     preset = f.read()
@@ -75,9 +93,6 @@ table = {}
 for x in preset.split('\n'):
     row = x.split(' ')
     table[row[0]] = row[1:]
-
-def path_replace(path, part):
-    return path.replace(part.encode(), os.path.join(PRESSURE_PATH, part[1:]).encode())
 
 for k, v in dumpenvs:
     try:
@@ -91,12 +106,19 @@ for k, v in dumpenvs:
             raise NotImplementedError()
         case 'd':
             pass
+        case 'r':
+            final_env[k] = ' '.join(t[1:])
+        case 'a':
+            final_env[k] = v + ' '.join(t[1:])
+        case 'p':
+            final_env[k] = ' '.join(t[1:]) + v
         case _:
             raise NotImplementedError()
 
-final_env[b'LANG'] = args.lang or b'ja_JP.UTF-8'
+final_env[b'LANG'] = args.lang
 final_env[b'STEAM_COMPAT_DATA_PATH'] = os.path.expanduser('~/wineprefix/' + PFX_TO_BE_MADE)
 final_env[b'WINEPREFIX'] = os.path.expanduser('~/wineprefix/' + PFX_TO_BE_MADE + '/pfx')
+final_env[b'WINE_GST_REGISTRY_DIR'] = os.path.expanduser('~/wineprefix/' + PFX_TO_BE_MADE + '/gstreamer-1.0')
 
 print(final_env)
 
@@ -106,10 +128,13 @@ proc_env.update(final_env)
 if args.r:
     proc_args = args_r_remain
 else:
-    proc_args = [os.path.join(proton_root, 'files', 'bin', 'wine')] + args.args
+    winepath = os.path.join(proton_root, 'files', 'bin', 'wine')
+    if not os.path.exists(winepath):
+        winepath = os.path.join(proton_root, 'dist', 'bin', 'wine')
+    proc_args = [winepath] + args.args
 
 import pickle
 with open(os.path.join(tool_path, 'inner', 'arg-pickle'), 'wb') as f:
     pickle.dump([proc_args, proc_env], f)
 
-subprocess.run([SPIDER_PATH, os.path.join(tool_path, 'inner', 'my-proton-inner.py')])
+subprocess.run([SNIPER_PATH, os.path.join(tool_path, 'inner', 'my-proton-inner.py')])
--- my-proton-tools/simpler-way.sh
+++
@@ -1,3 +0,0 @@
-PFX_TO_BE_MADE=mypfx
-PROTON_PATH="$HOME/.local/share/Steam/steamapps/common/Proton - Experimental"
-LANG=ja_JP.UTF-8 STEAM_COMPAT_CLIENT_INSTALL_PATH=$HOME/.local/share/Steam STEAM_COMPAT_DATA_PATH=$HOME/wineprefix/$PFX_TO_BE_MADE $HOME/.local/share/Steam/steamapps/common/SteamLinuxRuntime_sniper/run-in-sniper "$PROTON_PATH/proton" run $@
