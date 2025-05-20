67e0903b0772f2a292c89e599abaa4e50817621c
tora <kawarusosu@zoho.com>
2025-05-21 02:30:35 +0900

    add README; set def lng to en

--- whisper-cpp-timecode-adjust/02_whisper.bash
+++ whisper-cpp-timecode-adjust/02_whisper.bash
@@ -7,4 +7,4 @@ cd "$SCRIPT_DIR"
 
 export LD_LIBRARY_PATH="$SCRIPT_DIR/bin/lib:$SCRIPT_DIR/bin/lib64"
 
-find ./orig/ -name "*.wav" | xargs -n1 -I{} bash -c "./bin/bin/whisper-cli -m ./whisper.cpp/models/ggml-large-v3.bin -l ja -f '{}' > '{}.txt'"
+find ./orig/ -name "*.wav" | xargs -n1 -I{} bash -c "./bin/bin/whisper-cli -m ./whisper.cpp/models/ggml-large-v3.bin -l en -f '{}' > '{}.txt'"
